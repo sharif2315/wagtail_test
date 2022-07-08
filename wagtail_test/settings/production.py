957ce1a2c836d3e0d2c74fc9920c0864eae121c3
Sharif Ahmed <sharif2315@gmail.com>
2022-07-08 10:56:22 +0100

    updating settings

--- wagtail_test/settings/production.py
+++ wagtail_test/settings/production.py
@@ -3,7 +3,7 @@ from django.core.management.utils import get_random_secret_key
 
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', get_random_secret_key())
-ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', '127.0.0.1', 'localhost').split(',')
+ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', '127.0.0.1, localhost').split(',')
 
 try:
     from .local import *
