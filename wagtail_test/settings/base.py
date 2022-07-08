8d0a202b64730e841dd2e57f944bc8c54d4e151d
Sharif Ahmed <sharif2315@gmail.com>
2022-07-08 14:39:49 +0100

    updating settings

--- wagtail_test/settings/base.py
+++ wagtail_test/settings/base.py
@@ -165,7 +165,7 @@ STATICFILES_DIRS = [
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # STATIC_ROOT = os.path.join(BASE_DIR, "static")
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATIC_URL = "/static/"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
--- wagtail_test/settings/dev.py
+++ wagtail_test/settings/dev.py
@@ -1,15 +1,22 @@
 from .base import *
 from django.core.management.utils import get_random_secret_key
+import os
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', 'False') == 'True'
+# DEBUG = os.getenv('DEBUG', 'False') == 'True'
+
+if not os.getenv('DEBUG'):
+    DEBUG = True
+else:
+    DEBUG = False
+print('DEBUG: ', DEBUG)
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', get_random_secret_key())
 ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', '127.0.0.1, localhost').split(',')
 
 # SECURITY WARNING: define the correct hosts in production!
-ALLOWED_HOSTS = ["*"]
+# ALLOWED_HOSTS = ["*"]
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
